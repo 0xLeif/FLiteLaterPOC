5136cbf2c9e906df66c51f6d9f164b4382d0677d
zmeriksen <zmeriksen@icloud.com>
2020-10-02 15:43:38 -0500

    Initial Commit

---
+++ FLiteLaterPOC/FLiteLaterPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLiteLaterPOCApp.swift
+//  FLiteLaterPOC
+//
+//  Created by Zach Eriksen on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct FLiteLaterPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
